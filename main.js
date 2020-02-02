2597b0fddfef6415dc32455af4f00451e30f71df
Grzegorz Zielinski <gregoryblack11@gmail.com>
2020-02-02 12:08:14 +0100

    feature:BEM on my website

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const age=31;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const a=document.querySelector('.sectionHeader');
+console.log(a);
+
+const h=document.querySelectorAll('h3');
+console.log(h[3]);
+
+const about=document.querySelector('.about');
+console.log(about.innerHTML);
+//about.innerHTML='Pozdrawiam wszystkich czytajacych';

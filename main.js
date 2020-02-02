5bac7e760a6a2248b10eb4181c8fe81cb5062c93
Grzegorz Zielinski <gregoryblack11@gmail.com>
2020-02-02 15:59:40 +0100

    feature:code after visit DOM

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
 
-const a=document.querySelector('.sectionHeader');
-console.log(a);
+const title = document.querySelector('.section__title');
+console.log(title);
 
-const h=document.querySelectorAll('h3');
-console.log(h[3]);
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs); //tu wypisze all jako Note list
+console.log(paragraphs[0]); // tu wypisze par. 1
 
-const about=document.querySelector('.about');
-console.log(about.innerHTML);
-//about.innerHTML='Pozdrawiam wszystkich czytajacych';
+
+const section = document.querySelector('.section__title--js');
+console.log(section.innerHTML);
+section.innerHTML=`<i>Nazywam</i> się ${name} i mam ${age} lat`; //mozemy wrzucić znazniki html !!!
+
+//section.innerHTML='Pozdrawiam <strong> wszystkich </strong> czytających';
+
+//section.innerHTML = 'What a mess in my DOM'; //JS nadpisuje mi tresc ktora jest w HTML
